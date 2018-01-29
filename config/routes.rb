c94f00fbf30c7cdabbcfb93b7c044f6bb048c38b
yasuo- <breitling1216@gmail.com>
2018-01-29 18:01:11 +0900

    add Sessioncontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
+
   root 'static_pages#home'
 #  get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'# , as: 'helf'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-  get '/signup', to: 'users#create'
+  get '/login', to: 'session#new'
+  get '/login', to: 'session#create'
+  get '/logout', to: 'session#destroy'
   resources :users
 end
