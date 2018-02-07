15101a1e7720c93d18f87cc5ae7111cbbbc07726
yasuo- <breitling1216@gmail.com>
2018-02-07 14:06:24 +0900

    認可: 正しいユーザーの要求

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -44,11 +45,18 @@ class UsersController < ApplicationController
     end
 
     # before アクション
+
     # ログイン済みユーザーかどうか確認
     def logged_in_user
       unless logged_in?
         flash[:danger] = 'Please log in'
         redirect_to login_url
       end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user) # current_user?(@user) 同じ(@user == current_user)
     end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = 'Ruby on Rails Tutorial Sample App'
     @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test 'should get new' do
@@ -26,4 +27,22 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
+
+  # 間違ったユーザーが編集しようとしたときのテスト(edit)
+  test 'should redirect edit when logged in as wrong user' do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  # 間違ったユーザーが編集しようとしたときのテスト(update)
+  test 'should redirect update when logged in as wrong user' do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email }}
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
 end
