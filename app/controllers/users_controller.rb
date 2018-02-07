1b00666ab2d9603284e4be2eee34beefd9b1d925
yasuo- <breitling1216@gmail.com>
2018-02-07 15:38:36 +0900

    Finish user edit, update, index, and destroy actions 認可: 管理者による ユーザーの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     # @users = User.all -> all
@@ -42,6 +43,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = 'User defeted'
+    redirect_to users_url
+  end
+
   private
 
     def user_params
@@ -64,5 +71,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user) # current_user?(@user) 同じ(@user == current_user)
+    end
+
+    # 管理者(Admin)かどうか
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -46,8 +46,35 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test 'should redirect update when logged in as wrong user' do
     log_in_as(@other_user)
     patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email }}
+                                              email: @user.email } }
     assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  # admin属性の変更が禁止されていることをテストする
+  test 'should not allow the admin attribute to be edited via the web' do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: { user: { password: 'password',
+                                                    password_confirmation: 'password',
+                                                    admin: true } }
+    # assert_not @other_user.admin?
+  end
+
+  # 管理者権限の制御をアクションレベルでテストする
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  # 管理者権限の制御をアクションレベルでテストする (Adminではない)
+  test 'should redirect destroy when logged in as a non-admin' do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
     assert_redirected_to root_url
   end
 
